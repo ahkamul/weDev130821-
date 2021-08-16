2113613d9cdc0f4e8cbdaa30d4688b05bc199ec6
ahkamul <sasraishougat@gmail.com>
2021-08-16 16:25:51 +0600

    code written on study js from w3School (first to js assignment)

---
+++ index.js
@@ -0,0 +1,34 @@
+// let x = "Amima Hoque"
+
+// let x = a 
+
+// var x = "Amima Hoque"
+
+// //var x = a 
+
+// console.log(x)
+
+// var x = 10;
+
+// x += 5; // i.e x = x + 5
+
+// console.log(x);
+
+// var a = 20;
+// var b = 30;
+// var x = a + b;
+
+// console.log(x);
+
+// var a1 = 30;
+// var b1 = 40;
+// var x1 = a1 + b1;
+// var y = (2 + 3) * x1;
+
+// console.log(y);
+
+var x = 50;
+//x += 5;
+x -= 5;
+
+console.log(x);
